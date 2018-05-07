a420dc2b9f2c7e2673c6878ede3b1393f4c7456e
Hannes Mehnert <hannes@mehnert.org>
2018-05-07 23:57:35 +0200

    phase II in mirage-unikernel land

--- mirage/config.ml
+++ mirage/config.ml
@@ -37,14 +37,26 @@ let packages = [
   package "logs" ;
   package "cohttp-mirage" ;
   package "letsencrypt" ;
+  package ~sublibs:[ "mirage" ; "server" ] "udns" ;
+  package "hex" ;
 ]
 
 let client =
   foreign ~deps:[abstract nocrypto] ~keys ~packages "Unikernel.Client" @@
-  stackv4 @-> pclock @-> resolver @-> conduit @-> job
+  random @-> pclock @-> mclock @-> time @-> stackv4 @-> resolver @-> conduit @-> job
+
+let address =
+  let network = Ipaddr.V4.Prefix.of_address_string_exn "10.0.42.10/24"
+  and gateway = Ipaddr.V4.of_string "10.0.42.1"
+  in
+  { network ; gateway }
+
+let net =
+  if_impl Key.is_unix
+    (socket_stackv4 [Ipaddr.V4.any])
+    (static_ipv4_stack ~config:address ~arp:farp default_network)
 
 let () =
-  let stack = generic_stackv4 default_network in
-  let res_dns = resolver_dns stack in
-  let conduit = conduit_direct stack in
-  register "letsencrypt-client" [ client $ stack $ default_posix_clock $ res_dns $ conduit ]
+  let res_dns = resolver_dns net in
+  let conduit = conduit_direct net in
+  register "letsencrypt-client" [ client $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ net $ res_dns $ conduit ]
--- mirage/unikernel.ml
+++ mirage/unikernel.ml
@@ -2,8 +2,10 @@ open Mirage_types_lwt
 
 open Lwt.Infix
 
-module Client (S  : STACKV4) (CL : PCLOCK) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) = struct
+module Client (R : RANDOM) (P : PCLOCK) (M : MCLOCK) (T : TIME) (S : STACKV4) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S)= struct
   module Acme = Letsencrypt.Client.Make (Cohttp_mirage.Client)
+
+  module Dns = Dns_mirage.Make(R)(P)(M)(T)(S)
 
   (* goes along, generates a private key, and a csr with hostname, the initiates
    http connection to let's encrypt staging, dns challenge, nsupdate, retrieves
@@ -13,7 +15,7 @@ module Client (S  : STACKV4) (CL : PCLOCK) (RES: Resolver_lwt.S) (CON: Conduit_m
     let g = Nocrypto.Rng.(create ~seed (module Generators.Fortuna)) in
     Nocrypto.Rsa.generate ~g 4096
 
-  let start stack clock res ctx _ =
+  let start' _random pclock _mclock _ stack res ctx _ =
     let name, dnskey =
       match Astring.String.cut ~sep:":" (Key_gen.dns_key ()) with
       | None -> invalid_arg "couldn't parse dnskey"
@@ -31,7 +33,7 @@ module Client (S  : STACKV4) (CL : PCLOCK) (RES: Resolver_lwt.S) (CON: Conduit_m
       | Ok () -> Lwt.return (Ok ())
       | Error e -> Lwt.return (Error (Fmt.to_to_string S.UDPV4.pp_error e))
     in
-    let now = Ptime.v (CL.now_d_ps clock) in
+    let now = Ptime.v (P.now_d_ps pclock) in
     let solver = Letsencrypt.Client.default_dns_solver now send name dnskey in
     let sleep () = OS.Time.sleep_ns (Duration.of_sec 3) in
     Conduit_mirage.with_tls ctx >>= fun ctx ->
@@ -41,7 +43,7 @@ module Client (S  : STACKV4) (CL : PCLOCK) (RES: Resolver_lwt.S) (CON: Conduit_m
     | Ok t ->
       Acme.sign_certificate ~ctx ~solver t sleep csr >|= function
       | Error e -> Logs.err (fun m -> m "error %s" e)
-      | Ok cert -> Logs.info (fun m -> m "certificate: %s" cert)
+      | Ok cert -> Logs.info (fun m -> m "certificate: %s" (Cstruct.to_string @@ X509.Encoding.Pem.Certificate.to_pem_cstruct1 cert))
 
 
   (* part II:
@@ -67,4 +69,145 @@ module Client (S  : STACKV4) (CL : PCLOCK) (RES: Resolver_lwt.S) (CON: Conduit_m
        parsed as csr with a domain name we have keys for (or update uses the
        right key)
  *)
+
+  let start _random pclock mclock _ stack res ctx _ =
+    let key_name, dnskey =
+      match Astring.String.cut ~sep:":" (Key_gen.dns_key ()) with
+      | None -> invalid_arg "couldn't parse dnskey"
+      | Some (name, key) ->
+        match Dns_name.of_string ~hostname:false name, Dns_packet.dnskey_of_string key with
+        | Error _, _ | _, None -> invalid_arg "failed to parse dnskey"
+        | Ok name, Some dnskey -> (name, dnskey)
+    in
+    let account_key = gen_rsa (Key_gen.account_key_seed ()) in
+    let send data =
+      let dst = Key_gen.dns_server () in
+      Logs.debug (fun m -> m "writing to %a: %a" Ipaddr.V4.pp_hum dst Cstruct.hexdump_pp data) ;
+      S.UDPV4.write ~dst ~dst_port:53 (S.udpv4 stack) data >>= function
+      | Ok () -> Lwt.return (Ok ())
+      | Error e ->
+        Logs.err (fun m -> m "failed to send nsupdate due to %a" S.UDPV4.pp_error e) ;
+        Lwt.return (Error (Fmt.to_to_string S.UDPV4.pp_error e))
+    in
+    Conduit_mirage.with_tls ctx >>= fun ctx ->
+    let ctx = Cohttp_mirage.Client.ctx res ctx in
+    Acme.initialise ~ctx ~directory:(Uri.of_string "https://acme-staging.api.letsencrypt.org/directory") account_key >>= function
+    | Error e -> Logs.err (fun m -> m "error %s" e) ; Lwt.return_unit
+    | Ok le ->
+      let t =
+        let data =
+          (* all we need is: key (do we need soa for zone? maybe?) *)
+          Dns_trie.insert key_name Dns_map.(V (K.Dnskey, [ dnskey ]))
+            Dns_trie.empty
+        in
+        UDns_server.Primary.create ~a:[UDns_server.tsig_auth]
+          ~tsig_verify:Dns_tsig.verify ~tsig_sign:Dns_tsig.sign
+          ~rng:R.generate data
+      in
+      let state = ref t in
+      let in_flight = ref Astring.String.Set.empty in
+      Logs.info (fun m -> m "initialised lets encrypt") ;
+
+      let handle_csrs () =
+
+        let react_change name (_, tlsas) () =
+          let is_csr t =
+            t.Dns_packet.tlsa_selector = Dns_enum.Tlsa_selector_private &&
+            t.Dns_packet.tlsa_matching_type = Dns_enum.Tlsa_no_hash
+          and is_cert t =
+            t.Dns_packet.tlsa_selector = Dns_enum.Tlsa_full_certificate &&
+            t.Dns_packet.tlsa_matching_type = Dns_enum.Tlsa_no_hash &&
+            t.Dns_packet.tlsa_cert_usage = Dns_enum.Domain_issued_certificate
+          and matches csr cert =
+            (* parse csr, parse cert: match public keys, match validity of cert *)
+            Logs.warn (fun m -> m "not yet implemented: whether csr matches cert") ;
+            true
+          in
+          match List.filter is_csr tlsas, List.filter is_cert tlsas with
+          | [], _ -> Logs.info (fun m -> m "no private selector")
+          | [csr], [cert] when matches csr cert ->
+            Logs.info (fun m -> m "cert exists for csr")
+          | [tlsa], _ ->
+            begin
+              match X509.Encoding.parse_signing_request tlsa.Dns_packet.tlsa_data with
+              | exception e ->
+                Logs.err (fun m -> m "couldn't parse signing request: %s"
+                             (Printexc.to_string e))
+              | None -> Logs.err (fun m -> m "couldn't parse signing request")
+              | Some csr ->
+                match List.find (function `CN _ -> true | _ -> false) (X509.CA.info csr).X509.CA.subject with
+                | exception Not_found -> Logs.err (fun m -> m "cannot find name of signing request")
+                | `CN nam ->
+                  begin match Dns_name.of_string nam with
+                    | Error (`Msg msg) -> Logs.err (fun m -> m "error %s while creating domain name of %s" msg nam)
+                    | Ok dns_name ->
+                      if not (Dns_name.equal dns_name name) then
+                        Logs.err (fun m -> m "csr cn %a doesn't match dns %a" Dns_name.pp dns_name Dns_name.pp name)
+                      else if Astring.String.Set.mem nam !in_flight then
+                        Logs.err (fun m -> m "request with %s already in-flight" nam)
+                      else begin
+                        in_flight := Astring.String.Set.add nam !in_flight ;
+                        (* TODO check csr commen name with name *)
+                        (* request new cert in async *)
+                        Lwt.async (fun () ->
+                            let sleep () = OS.Time.sleep_ns (Duration.of_sec 3) in
+                            let now = Ptime.v (P.now_d_ps pclock) in
+                            let solver = Letsencrypt.Client.default_dns_solver now send key_name dnskey in
+                            Acme.sign_certificate ~ctx ~solver le sleep csr >|= function
+                            | Error e -> Logs.err (fun m -> m "error %s" e)
+                            | Ok cert ->
+                              let certificate = X509.Encoding.cs_of_cert cert in
+                              Logs.info (fun m -> m "certificate received") ;
+                              let trie = UDns_server.Primary.data !state in
+                              match Dns_trie.lookup_direct name Dns_map.K.Tlsa trie with
+                              | Ok (ttl, tlsas) ->
+                                let tlsa = { Dns_packet.tlsa_cert_usage = Dns_enum.Domain_issued_certificate ;
+                                             tlsa_selector = Dns_enum.Tlsa_full_certificate ;
+                                             tlsa_matching_type = Dns_enum.Tlsa_no_hash ;
+                                             tlsa_data = certificate }
+                                in
+                                let others = List.filter (fun t -> not (is_cert t)) tlsas in
+                                let trie = Dns_trie.insert name Dns_map.(V (K.Tlsa, (ttl, tlsa :: others))) trie in
+                                state := UDns_server.Primary.with_data !state trie ;
+                                in_flight := Astring.String.Set.remove nam !in_flight ;
+                              | Error e ->
+                                Logs.err (fun m -> m "couldn't find tlsa for %a: %a" Dns_name.pp name Dns_trie.pp_e e))
+                      end
+                  end
+                | _ -> Logs.err (fun m -> m "cannot find common name of signing request")
+            end
+          | _, _ -> Logs.err (fun m -> m "not prepared for this task")
+        in
+
+        match Dns_trie.folde Dns_name.root Dns_map.K.Tlsa (UDns_server.Primary.data !state) react_change () with
+        | Ok () -> ()
+        | Error e -> Logs.warn (fun m -> m "error %a while folding" Dns_trie.pp_e e)
+      in
+
+      let tcp_cb flow =
+        let dst_ip, dst_port = S.TCPV4.dst flow in
+        Logs.info (fun m -> m "tcp connection from %a:%d" Ipaddr.V4.pp_hum dst_ip dst_port) ;
+        let f = Dns.of_flow flow in
+        let rec loop () =
+          Dns.read_tcp f >>= function
+          | Error () -> Lwt.return_unit
+          | Ok data ->
+            let now = Ptime.v (P.now_d_ps pclock) in
+            let elapsed = M.elapsed_ns mclock in
+            let t, answer, _ = UDns_server.Primary.handle !state now elapsed `Tcp dst_ip data in
+            state := t ;
+            (* here we fold over TLSA *)
+            handle_csrs () ;
+            match answer with
+            | None -> Logs.warn (fun m -> m "empty answer") ; loop ()
+            | Some answer ->
+              Dns.send_tcp flow answer >>= function
+              | Ok () -> loop ()
+              | Error () -> Lwt.return_unit
+        in
+        loop ()
+      in
+      S.listen_tcpv4 stack ~port:53 tcp_cb ;
+      S.listen stack
+
 end
